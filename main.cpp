2c99c7c8203a0ceae686a449610dd9e85faae9db
JinWang <jwang186@jhu.edu>
2023-04-02 00:29:38 -0700

    fixed some bugs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <fstream>
+#include<fstream>
 #include<iostream>
 #include<string>
 #include<vector>
@@ -44,7 +44,8 @@ class Args {
         float dy = 0.006;
         string dataFile = "dataFile.txt";
         int numOfHeaderLines = 3;
-        string outFile = "out.txt";       
+        string outFile = "out.txt";
+        bool hasRealP = false;       
 
     public:
         Args() {
@@ -77,6 +78,8 @@ class Args {
                 numOfHeaderLines = std::stoi(pair[1]);
             } else if (key == "outFile") {
                 outFile = pair[1];
+            } else if (key == "hasRealP") {
+                hasRealP = pair[1] == "true";
             } else {
                 cout<<"Invalid argument: " << key << "=" << pair[1]<<endl;;
             }
@@ -105,6 +108,7 @@ class Args {
             cout<<"dataFile="<<dataFile<<endl;
             cout<<"numOfHeaderLines="<<numOfHeaderLines<<endl;
             cout<<"outFile="<<outFile<<endl;
+            cout<<"hasRealP="<<hasRealP<<endl;
         }    
 };
 
@@ -115,8 +119,13 @@ class Data {
         vector<vector<float>> x;
         vector<vector<float>> y;
         vector<vector<float>> p;
+        vector<vector<float>> pReal;
         vector<vector<float>> pNew;
         vector<vector<float>> pCount;
+        float pAvgReal = 0;
+        float pAvg = 0;
+        float pDiff = 0;
+        float pError = 0;
 
         Args* args;        
 
@@ -124,21 +133,37 @@ class Data {
         Data(Args* args) {
             this->args = args;
             for(int i = 0; i < this->args->Imax; i++) {
-                x.push_back(vector<float>(this->args->Jmax));
-                y.push_back(vector<float>(this->args->Jmax));
-                dudt.push_back(vector<float>(this->args->Jmax));
-                dvdt.push_back(vector<float>(this->args->Jmax));
-                p.push_back(vector<float>(this->args->Jmax));
-                pNew.push_back(vector<float>(this->args->Jmax));
-                pCount.push_back(vector<float>(this->args->Jmax));
+                x.push_back(vector<float>(this->args->Jmax, 0));
+                y.push_back(vector<float>(this->args->Jmax, 0));
+                dudt.push_back(vector<float>(this->args->Jmax, 0));
+                dvdt.push_back(vector<float>(this->args->Jmax, 0));
+                p.push_back(vector<float>(this->args->Jmax, 0));
+                pReal.push_back(vector<float>(this->args->Jmax, 0));
+                pNew.push_back(vector<float>(this->args->Jmax, 0));
+                pCount.push_back(vector<float>(this->args->Jmax, 0));
             }
 
             // generate some random numbers for testing, can remove it.
             srand((unsigned) time(NULL));
             for(int i = 0; i < this->args->Imax; i++) {
                 for(int j = 0; j < args->Jmax; j++) {
-                    dudt[i][j] = rand()/(float (rand()));
-                    dvdt[i][j] = rand()/(float (rand()));
+                    x[i][j] = i*args->dx;
+                    y[i][j] = j*args->dy;
+                    pReal[i][j] = exp(sinf((x[i][j] + y[i][j])*1.0/(args->Imax + args->Jmax)*2*PI));
+                    pAvgReal += pReal[i][j];
+                }
+            }
+            pAvgReal = pAvgReal/args->Imax/args->Jmax;
+
+            // calculate dpdx and dpdy, notice rho*dudt = - dpdx
+            for(int i = 0; i < this->args->Imax; i++) {
+                for(int j = 0; j < args->Jmax; j++) {
+                    int is = (i == 0 ? 0 : i - 1);
+                    int ie = (i == args->Imax - 1 ? args->Imax - 1 : i + 1);
+                    int js = (j == 0 ? 0 : j - 1);
+                    int je = (j == args->Jmax - 1 ? args->Jmax - 1 : j + 1);
+                    dudt[i][j] = -1/args->rho*(pReal[ie][j] - pReal[is][j])/((ie - is)*args->dx) + (((float) rand()) / (float) RAND_MAX - 0.5)/10; 
+                    dvdt[i][j] = -1/args->rho*(pReal[i][je] - pReal[i][js])/((je - js)*args->dy) + (((float) rand()) / (float) RAND_MAX - 0.5)/10; 
                 }
             }
         }
@@ -170,6 +195,9 @@ class Data {
             y[i][j] = std::stof(pointWiseData[1]);
             dudt[i][j] = std::stof(pointWiseData[2]);
             dvdt[i][j] = std::stof(pointWiseData[3]);
+            if (args->hasRealP) {
+                pReal[i][j] = std::stof(pointWiseData[4]);
+            }
         }
 
 
@@ -177,9 +205,21 @@ class Data {
         void writePressureToFile() {
             ofstream outdata;
             outdata.open(args->outFile);
+            outdata<<"TITLE=\"Pressure integrated from Omni2D\""<<endl;
+            if (args->hasRealP) {
+                outdata<<"VARIABLES = \"X\",\"Y\",\"Dudt\",\"DvDt\",\"Pcal\",\"Preal\",\"Count\""<<endl;
+            } else {
+                outdata<<"VARIABLES = "<< "\"X\",\"Y\",\"Dudt\",\"DvDt\",\"Pcal\",\"Count\""<<endl;
+            }
+            outdata<<"ZONE I="<<args->Imax<<" "<<"J="<<args->Jmax<<endl;
+
             for(int i = 0; i < args->Imax; i++) {
                 for(int j = 0; j < args->Jmax; j++) {
-                    outdata<<x[i][j]<<" "<<y[i][j]<<" "<<dudt[i][j]<<" "<<dvdt[i][j]<<" "<<p[i][j]<<" "<<pCount[i][j]<<endl;
+                    if (args->hasRealP) {
+                        outdata<<x[i][j]<<" "<<y[i][j]<<" "<<dudt[i][j]<<" "<<dvdt[i][j]<<" "<<p[i][j] - pAvg + pAvgReal<<" "<<pReal[i][j]<<" "<<pCount[i][j]<<endl;
+                    } else {
+                        outdata<<x[i][j]<<" "<<y[i][j]<<" "<<dudt[i][j]<<" "<<dvdt[i][j]<<" "<<p[i][j] - pAvg<<" "<<pCount[i][j]<<endl;
+                    }
                 }
             }
             outdata.close();
@@ -188,38 +228,56 @@ class Data {
         void parallelLinesOdiMultiple() {
             cout<<"Processing starts"<<endl;
             for(int i = 0; i < args->numOfIterations ; i++) {
-                parallellinesOdiOnce(i == args->numOfIterations -1);
-                cout<<"Iteration: "<<i<<endl;
+                parallellinesOdiOnce(i == args->numOfIterations - 1);
+                if (args->hasRealP) {
+                    cout<<"Iteration: "<<i<<" abs(p - pReal): "<<pError << " abs(pN - p):"<<pDiff<<endl;
+                } else {
+                    cout<<"Iteration: "<<i<< " abs(pN - p):"<<pDiff<<endl;
+                }
             }
         }
 
         void parallellinesOdiOnce(bool isLastIeration) {
             int numOfParallelLines = max(args->Imax, args->Jmax)*1.414/args->lineSpacing;
             for(int angleId = 0; angleId < args->numOfAngles; angleId++) {
-                float angle = (angleId * 2 * PI)/ args->numOfAngles;
+                float angle = (1.0 * angleId * 2 * PI)/ args->numOfAngles;
                 float kx = cosf(angle);
                 float ky = sinf(angle);
                 for(int lineId = 0; lineId < numOfParallelLines; lineId++) {
                     float ic = sinf(angle)*(lineId - numOfParallelLines / 2)*args->lineSpacing + (args->Imax - 1) * 0.5;
                     float jc = cosf(angle)*(lineId - numOfParallelLines / 2)*args->lineSpacing + (args->Jmax - 1) * 0.5;
                     vector<point*> crossingPoints = getCrossingPointsOnBoundary(kx, ky, ic, jc);
-                    if (crossingPoints.size() >= 2) {
+                    if (crossingPoints.size() == 2) {
                         bodyIntegralAlongALine(kx, ky, ic, jc, crossingPoints[0]->x, crossingPoints[0]->y, crossingPoints[1]->x, crossingPoints[1]->y);
                     }
                 }
             }
             // averaging
+            pAvg = 0;
+            pDiff = 0;
+            pError = 0;
             for(int i = 0; i < args->Imax; i++) {
                 for(int j = 0; j < args->Jmax; j++) {
+                    pAvg += p[i][j];
                     if (pCount[i][j] > 0) {
+                        pDiff += abs(pNew[i][j]/pCount[i][j] - p[i][j]);
                         p[i][j] = pNew[i][j]/pCount[i][j];
                         if (!isLastIeration) {
-                            pNew[i][j] = p[i][j];
-                            pCount[i][j] = 1; 
+                            pNew[i][j] = 0;
+                            pCount[i][j] = 0; 
                         }
                     }
                 }
             }
+            pAvg = pAvg/args->Imax/args->Jmax;
+          
+            for(int i = 0; i < args->Imax; i++) {
+                for(int j = 0; j < args->Jmax; j++) {
+                    pError += abs(p[i][j] - pAvg - (pReal[i][j] - pAvgReal));
+                }
+            }
+            pDiff = pDiff/args->Imax/args->Jmax;
+            pError = pError/args->Imax/args->Jmax;
         }
 
     public:
@@ -282,17 +340,23 @@ class Data {
         float bodyIntegralAlongALine(float kx, float ky, float ic, float jc, int iin, int jin, int iout, int jout) {
             // line: (i - ic)/kx = (j - jc)/ky
             if (kx*(iout - iin) + ky*(jout - jin) < 0) {
-                swap(iout, iin);
-                swap(jout, jin);
+                int tmp = iout;
+                iout = iin;
+                iin = tmp;
+                tmp = jout;
+                jout = jin;
+                jin = tmp;
 	        }
             int ilast, jlast, inext1, jnext1, inext2, jnext2;
             ilast = iin;
             jlast = jin;
 
             float pint = 0;
-            bool flag = 0;
+            int choose = -1;
+            pNew[ilast][jlast] += p[ilast][jlast];
+            pCount[ilast][jlast] += 1;
             do {
-                flag = 0;
+                choose = -1;
 		        if (ilast < iout) {
 			        inext1 = ilast + 1; jnext1 = jlast;
 		        } else if (ilast == iout) {
@@ -301,7 +365,7 @@ class Data {
 			        inext1 = ilast - 1; jnext1 = jlast;
 		        }
 
-                if (jlast<jout) {
+                if (jlast < jout) {
 			        inext2 = ilast; jnext2 = jlast + 1; 
 		        } else if (jlast == jout) {
 			        inext2 = ilast; jnext2 = jlast - 60000; 
@@ -315,22 +379,33 @@ class Data {
                 float d1 = abs(ky*inext1 - ky*ic - kx*jnext1 + kx*jc) / sqrt(kx*kx + ky*ky);
                 float d2 = abs(ky*inext2 - ky*ic - kx*jnext2 + kx*jc) / sqrt(kx*kx + ky*ky);
                 // integral marching towards minimal distance
-                if (d1 <= d2 && inext1 >= 0 && inext1 < args->Imax) { 
-                    pint += -args->rho*(inext1 - ilast)*args->dx*0.5*(dudt[inext1][jnext1] + dudt[ilast][jlast]);
-                    pNew[inext1][jnext1] += p[iin][jin] + pint;
-                    pCount[inext1][jnext1]++;
-                    ilast = inext1;
-                    flag = 1;
-                } else if (d1 > d2 && jnext2 >= 0 && jnext2 < args->Jmax) {
-                    pint += -args->rho*(jnext2 - jlast)*args->dy*0.5*(dvdt[inext2][jnext2] + dvdt[ilast][jlast]);
-                    pNew[inext2][jnext2] += p[iin][jin] + pint;
-                    pCount[inext2][jnext2]++;
-                    jlast = jnext2;
-                    flag = 1;                    
+                if (abs(d1 - d2) < 1e-5) {
+                    if (inext1 >= 0 && inext1 < args->Imax) {
+                        choose = 1;
+                    } else if (jnext2 >= 0 && jnext2 < args->Jmax) {
+                        choose = 2;
+                    }
+                } else {
+                    if (d1 < d2 && inext1 >= 0 && inext1 < args->Imax) {
+                        choose = 1;
+                    } else if(d1 > d2 && jnext2 >= 0 && jnext2 < args->Jmax) {
+                        choose = 2;
+                    }
                 }
-
-            } while(abs(ilast - iout) + abs(jlast - jout) > 1e-5 && flag);
-            return flag ? pint : 0;
+                if (choose == 1) { 
+                    pint += -args->rho*(inext1 - ilast)*args->dx*0.5*(dudt[inext1][jnext1] + dudt[ilast][jlast]);
+                    pNew[inext1][jnext1] += (p[iin][jin] + pint);
+                    pCount[inext1][jnext1] += 1;
+                    ilast = inext1;
+                } else if (choose == 2) {
+                    pint += -args->rho*(jnext2 - jlast)*args->dy*0.5*(dvdt[inext2][jnext2] + dvdt[ilast][jlast]);
+                    pNew[inext2][jnext2] += (p[iin][jin] + pint);
+                    pCount[inext2][jnext2] += 1;
+                    jlast = jnext2;                 
+                }
+            } while(abs(ilast - iout) + abs(jlast - jout) > 1e-1 && choose != -1);
+            //if (choose == -1)cout<<"Error, wrong point"<<endl;
+            return choose != -1 ? pint : 0;
         }        
 };
 
